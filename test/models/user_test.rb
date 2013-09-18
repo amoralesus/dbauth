c18653ce06429ad89dc60c635df17dac6990b0e4
Alberto Morales <amorales@opnet.com>
2013-09-18 16:03:27 -0500

    adding rails app

---
+++ test/models/user_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  test "a user with a password should save" do
+    user = User.new(:username => 'one', :email => 'alberto@moralitos.com', :password => 'password')
+    user.save
+    puts user.inspect
+    assert_equal([], user.errors.full_messages, "Expected no errors but go #{user.errors.full_messages}")
+  end
+
+  test "a user should authenticate correctly" do
+    user = User.new(:username => 'testing', :email => 'other@other.com', :password => 'testing')
+    user.save!
+    assert(User.authenticate('testing', 'testing'), "did not authenticate, #{user.errors.full_messages.inspect}")
+  end
+end
