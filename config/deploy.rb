3493da77d9081a09465ab0cb71cad8ac0c53d234
moralesalberto <alberto@moralitos.com>
2013-09-30 17:11:19 -0400

    adding migration and restart to capistrano for passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,14 +20,15 @@ set :keep_releases, 5
 
 default_run_options[:pty] = true
 
+after "deploy:update_code", "deploy:migrate"
 
 server "204.197.210.139", :app, :web, :db, :primary => true
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
